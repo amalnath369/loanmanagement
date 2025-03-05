25e2e8f3b5aeda0dd6ba131f088a57eb99485e65
Amalnath <amalnaths640@gmail.com>
2025-03-05 10:05:38 +0530

    Fix database connection for Render

--- loanmangement/loanmangement/settings.py
+++ loanmangement/loanmangement/settings.py
@@ -13,6 +13,7 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,14 +80,9 @@ WSGI_APPLICATION = 'loanmangement.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),  
-        'USER': os.getenv('DB_USER'),  
-        'PASSWORD': os.getenv('DB_PASSWORD'),  
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': 
+        dj_database_url.config(default=os.getenv("DATABASE_URL"),conn_max_age=600)
+    
 }
 
 
